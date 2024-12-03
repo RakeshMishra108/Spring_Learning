df4add71cff3762853d774308ee21f2f71a4b967
“RakeshMishra108” <rakeshmishra1098@gmail.com>
2024-12-03 17:40:51 +0530

    second commit

---
+++ src/main/java/com/first/trial/Nehish_Practice/NehishPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.first.trial.Nehish_Practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NehishPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NehishPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/first/trial/Nehish_Practice/NehishPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.first.trial.Nehish_Practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NehishPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
